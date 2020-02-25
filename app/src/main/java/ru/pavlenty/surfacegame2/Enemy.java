7653f73f498a70b5ed3f3d40af9cac221a1275b9
thisdjango <tsarkova.92@mail.ru>
2020-02-25 14:28:28 +0300

    Initial commit

---
+++ app/src/main/java/ru/pavlenty/surfacegame2/Enemy.java
@@ -0,0 +1,77 @@
+package ru.pavlenty.surfacegame2;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+public class Enemy {
+    private Bitmap bitmap;
+    private int x;
+    private int y;
+    private int speed = 0;
+    private boolean boosting;
+    private final int GRAVITY = -10;
+    private int maxY;
+    private int minY;
+    private int maxX;
+
+    private Rect detectCollision;
+
+    public Enemy(Context context, int screenX, int screenY) {
+        speed = 1;
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.enemy);
+        maxY = screenY - bitmap.getHeight();
+        maxX = screenX - bitmap.getWidth();
+        minY = 0;
+        x = maxX;
+        y = (int) (Math.random() * (screenY + 1) + 50);
+        boosting = false;
+
+        detectCollision = new Rect(x, y, bitmap.getWidth(), bitmap.getHeight());
+    }
+//     public void stopBoosting() {
+//         boosting = false;
+//     }
+//     public void setBoosting() {
+//         boosting = true;
+//     }
+
+    public void update() {
+        x -= 5;
+
+
+        detectCollision.left = x;
+        detectCollision.top = y;
+        detectCollision.right = x + bitmap.getWidth();
+        detectCollision.bottom = y + bitmap.getHeight();
+
+    }
+
+
+    public Rect getDetectCollision() {
+        return detectCollision;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int my_y) {
+        y = my_y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
+
+
